1d69a71d9c37b8013dab2a0faf3ad52cffe40555
Akshay <akadelhi1994@gmail.com>
2017-06-10 04:14:44 -0700

    238. Product of Array Except Self

---
+++ Q_238.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> vec1(nums.size());   
+        
+        vec1[0] = 1;
+        vec1[nums.size()-1] = 1;
+        
+        int mul,i;
+        
+        mul = 1;
+        
+        for(i=1;i<nums.size();i++)
+        {
+            mul *= nums[i-1];
+            vec1[i] = mul;
+        }
+        
+        mul = 1;
+        
+        for(i=nums.size()-2;i>=0;i--)
+        {
+            mul *= nums[i+1];
+            vec1[i] *= mul;
+        }
+        
+        mul = 1;
+        
+        return vec1;
+        
+    }
+};
