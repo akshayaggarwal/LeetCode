bab2fea79eaec6b4eb2221b9f0cc48dd11ced92e
Akshay <akadelhi1994@gmail.com>
2017-06-25 23:49:13 -0700

    Search a 2D Matrix II

--- Q_240.cpp
+++ Q_240.cpp
@@ -1,14 +1,45 @@
 class Solution {
 public:
+
+   /* int findrow(vector<vector<int>>& matrix, int target){
+        
+        for(int i=0;i<matrix.size();i++){
+            if(target>=matrix[i][0] && target<=matrix[i][matrix[0].size()-1])
+                return i;
+        }
+        
+        return -1;
+        
+    }*/
+
+    bool findelem(vector<int> vec,int target){
+        
+        for(int i = 0;i<vec.size();i++){
+            if(vec[i] == target)
+                return true;
+        }
+        
+        return false;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        if(matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
         
         int i,j;
         
+        
         for(i=0;i<matrix.size();i++){
-            for(j=0;j<matrix[0].size();j++){
-                if(matrix[i][j] == target)
-                    return true;
-            }
+        
+        if(target>=matrix[i][0] && target<=matrix[i][matrix[0].size()-1]){
+            
+            if(findelem(matrix[i],target) == true)
+                return true;
+            
+        }
+            
+            
         }
         
         return false;
