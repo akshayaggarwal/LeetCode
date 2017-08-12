b55ec98e4e72b3f0d83d6575b13a41a79704ba3b
Akshay <akadelhi1994@gmail.com>
2017-08-11 21:51:27 -0700

    Increasing Triplet Subsequence

---
+++ Q_334.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+        int i,num1 = INT_MAX,num2 = INT_MAX;
+        
+        for(i=0;i<nums.size();i++){
+            if(nums[i]<=num1)
+                num1 = nums[i];
+            else if(nums[i]<=num2)
+                num2 = nums[i];
+            else return true;
+        }
+        
+        return false;
+        
+    }
+};
