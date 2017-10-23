6987b4a775c77ca6229f7ea557d256263532ace4
Akshay <akadelhi1994@gmail.com>
2017-10-23 00:43:05 -0700

    Sort Colors

--- Q_75.cpp
+++ Q_75.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
         int i,j = 0;
-        
         for(i=0;i<nums.size();i++){
             if(nums[i] == 0){
                 swap(nums[i],nums[j]);
                 j++;
             }
         }
-        
-         //       j = 0;
         for(i=0;i<nums.size();i++){
             if(nums[i] == 1){
                 swap(nums[i],nums[j]);
                 j++;
             }
         }
-        
-           //     j = 0;
-        /*for(i=0;i<nums.size;i++){
-            if(nums[i] == 0){
-                swap(nums[i],nums[j]);
-                j++;
-            }
-        }*/
-        
     }
 };
