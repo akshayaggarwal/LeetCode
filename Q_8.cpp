77100010b0999a7f1d03fd0cb1b8548a24fd5819
Akshay <akadelhi1994@gmail.com>
2017-10-22 20:53:31 -0700

    String to Integer (atoi)

--- Q_8.cpp
+++ Q_8.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        
-        if(str.size() == 0)
+        if(str.size()==0)
             return 0;
-            
-        double a = 0;
+        int st=0,i=0,flag = 0;
+        double num = 0;
         
-        int flag = 0,i;
-        
-        i = 0;
-        
-        while(str[i] == ' '){
+        while(i<str.size() && str[i] == ' ')
             i++;
-        }
-            
+        
+        while(i<str.size() && str[i] == '0')
+            i++;
+
         if(str[i] == '-'){
             flag = 1;
             i++;
         }
-        else if(str[i] == '+'){
-            flag = 0;
+        else if(str[i] == '+')
             i++;
-        }
         
-        for(;i<str.size();i++)
-        {
-            if(str[i]<48 || str[i] > 57)
+        while(str[i] == '0')
+            i++;
+        
+        for(;i<str.size();i++){
+            if(str[i]<'0' || str[i]>'9')
                 break;
-            a = a*10 + str[i]-48;
+            num = (num*10) + (str[i]-48);
+            
+            if(flag == 1 && (num*(-1)) < INT_MIN)
+                return INT_MIN;
+            if(flag == 0 && num>INT_MAX)
+                return INT_MAX;
         }
-        
         if(flag == 1)
-            a*=-1;
-            
-        if(a>INT_MAX)
-            return INT_MAX;
-            
-        if(a<INT_MIN)
-            return INT_MIN;    
-        
-        return a;
+            num *= -1;
+        return (int)num;
     }
 };
