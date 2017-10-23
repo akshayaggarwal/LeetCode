c4bf86a168e895bae4848adac3e53c1c895e6dce
Akshay <akadelhi1994@gmail.com>
2017-10-23 00:29:29 -0700

    3Sum

--- Q_15.cpp
+++ Q_15.cpp
@@ -1,55 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-    
-    
         vector<vector<int>> ret;
-        
         if(nums.size()<3)
             return ret;
-        
         sort(nums.begin(),nums.end());
-
-        int i,target,a,front,back,k = nums.size();
-
         
+        vector<int> myvec;
+        int i,j,target,a,front,back;
         for(i=0;i<nums.size()-1;i++){
-            
-            target = -nums[i];
-            
             front = i+1;
             back = nums.size()-1;
-            
-
+            target = -nums[i];
             while(front<back){
-                
+                myvec.clear();
                 a = nums[front]+nums[back];
-                if(a<target){
+                if(a<target)
                     front++;
-                }
                 else if(a>target)
                     back--;
                 else{
-                    vector<int> myvec;
+                    myvec.push_back(nums[i]);
                     myvec.push_back(nums[front]);
                     myvec.push_back(nums[back]);
-                    myvec.push_back(nums[i]);
-
                     ret.push_back(myvec);
-                    while(front<back && nums[front]==myvec[0])
+                    while(front<back&&nums[front] == myvec[1])
                         front++;
-                        
-                    while(front<back && nums[back]==myvec[1])
+                    while(front<back&&nums[back] == myvec[0])
                         back--;
-
                 }
             }
-            
             while(i<nums.size()&&nums[i]==-target)
                 i++;
             i--;
         }
-        
         return ret;
     }
 };
