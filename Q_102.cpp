4de74a392221ce1ae88e434f931f421c0c671374
Akshay <akadelhi1994@gmail.com>
2017-10-23 19:11:45 -0700

    Binary Tree Level Order Traversal

--- Q_102.cpp
+++ Q_102.cpp
@@ -10,65 +10,23 @@
 class Solution {
 public:
     
+    map<int,vector<int>> mymap;
+    
+    void fillmap(TreeNode* root,int level){
+        if(root == NULL)
+            return;
+        mymap[level].push_back(root->val);
+        fillmap(root->left,level+1);
+        fillmap(root->right,level+1);
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-      
-      int i,a;
-      
-      vector<vector<int>> ret;
-      TreeNode* root_temp;
-      
-      if(root == NULL)
-        return ret;
-        
-      int level = 1;
-      
-      ret.push_back(vector<int>());
-      ret[0].push_back(root->val);
-      
-      vector<TreeNode*> vec;
-      
-      vec.push_back(root);
-      
-      while(true)
-      {
-          a = vec.size();
-          for(i=0;i<a;i++) {
-              
-          root_temp = vec[i];
-          
-          if(root_temp->left == NULL && root_temp->right == NULL){
-           // printf("reached value is %d \n",root_temp->val);
-            continue;
-          }
-            
-          if(root_temp->left!=NULL)
-          {
-              if(ret.size() == level)
-                ret.push_back(vector<int>());
-              ret[level].push_back(root_temp->left->val);
-              vec.push_back(root_temp->left);
-          }
-          
-          if(root_temp->right!=NULL)
-          {
-              if(ret.size() == level)
-                ret.push_back(vector<int>());
-              ret[level].push_back(root_temp->right->val);
-              vec.push_back(root_temp->right);
-          }
-          
+        fillmap(root,0);
+        vector<vector<int>> ret;
+        map<int,vector<int>>::iterator it = mymap.begin();
+        while(it!=mymap.end()){
+            ret.push_back(it->second);  
+            it++;
         }
-        
-        
-        vec.erase(vec.begin(),vec.begin()+a);
-        level++;
-        
-       /* for(i=0;i<vec.size();i++){
-                printf("val is %d \n",vec[i]->val);
-        }*/
-        //printf("vec size2 is %d\n",vec.size());
-        if(vec.size() == 0)
-            return ret;
-      }
+        return ret;
     }
 };
