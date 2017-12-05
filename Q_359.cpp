e3567ff4e47851856ca36693a9b2c738f9b44ab6
Akshay <akadelhi1994@gmail.com>
2017-12-04 17:12:14 -0800

    Logger Rate Limiter

--- Q_359.cpp
+++ Q_359.cpp
@@ -5,27 +5,26 @@ public:
     map<string,int> mymap;
     
     Logger() {
-            
+        
     }
     
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     bool shouldPrintMessage(int timestamp, string message) {
-        
         if(mymap.find(message) != mymap.end()){
-            if(abs(timestamp - mymap[message]) >=10){
-                mymap[message] = timestamp;
+            if(timestamp-mymap[message]<10)
+                return false;
+            else{
+                mymap[message]=timestamp;
                 return true;
             }
-            else{
-                return false;
-            }
+                
         }
         else{
-            mymap[message] = timestamp;
-            return true;
-        }
+                mymap[message]=timestamp;
+                return true;
+        }   
     }
 };
 
