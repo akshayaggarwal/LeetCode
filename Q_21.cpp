9f72ded1fced1f59c38ea3bc7b394ab1e1c4ff4e
Akshay <akadelhi1994@gmail.com>
2017-12-26 12:19:25 -0800

    Merge Two Sorted Lists

--- Q_21.cpp
+++ Q_21.cpp
@@ -9,29 +9,40 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL)
+        if(l1==NULL)
             return l2;
-        else if(l2 == NULL)
+        else if(l2==NULL)
             return l1;
-        ListNode *head,*temp,*l1prev;
-        head = l1;
-        while(l1!=NULL && l2!=NULL){
-            if(l1->val >l2->val){
-                temp = new ListNode(0);
-                temp->val = l1->val;
-                l1->val = l2->val;
-                temp->next = l1->next;
-                l1->next = temp;
-                l2 = l2->next;
+        ListNode *ret,*prev,*prev2;
+        l1->val<l2->val?ret=l1:ret=l2;
+        while(l1!=NULL&&l2!=NULL){
+            if(l1->val<l2->val){
+                while(l1!=NULL&&l1->val<=l2->val){
+                    prev=l1;
+                    l1=l1->next;
+                }
+                if(l1==NULL){
+                    prev->next=l2;
+                    return ret;
+                }
+                else{
+                    prev->next=l2;
+                }
             }
             else{
-                l1prev = l1;
-                l1 = l1->next;
+                while(l2!=NULL&&l2->val<=l1->val){
+                    prev2=l2;
+                    l2=l2->next;
+                }
+                if(l2==NULL){
+                    prev2->next=l1;
+                    return ret;
+                }
+                else{
+                    prev2->next=l1;
+                }
             }
         }
-        if(l1 == NULL)
-            l1prev->next = l2;
-
-        return head;
+        return ret;
     }
 };
