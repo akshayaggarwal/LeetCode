15a80b1d3a9f2b7de2a8922e82d1df9c5a7fba9d
Akshay <akadelhi1994@gmail.com>
2018-01-06 20:23:56 -0800

    Letter Combinations of a Phone Number

--- Q_17.cpp
+++ Q_17.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    map<int,string> mymap;
-    
     vector<string> letterCombinations(string digits) {
-        vector<string> ret;
-        if(digits.size() == 0){
-            printf("here\n");
-            return ret;
-        }
-        mymap[2] = "abc";
-        mymap[3] = "def";
-        mymap[4] = "ghi";
-        mymap[5] = "jkl";
-        mymap[6] = "mno";
-        mymap[7] = "pqrs";
-        mymap[8] = "tuv";
-        mymap[9] = "wxyz";
-        string str = "";
+        map<char,string> mymap;
+        vector<string> myvec;
+        vector<string> ret1,ret2;
+        string str,temp;
         int i,j,k;
-        string temp;
-        ret.push_back("");
+        
+        if(digits.size()==0)
+            return ret1;
+        mymap['2']="abc";
+        mymap['3']="def";
+        mymap['4']="ghi";
+        mymap['5']="jkl";
+        mymap['6']="mno";
+        mymap['7']="pqrs";
+        mymap['8']="tuv";
+        mymap['9']="wxyz";
+        ret1.push_back("");
         for(i=0;i<digits.size();i++){
-            if(digits[i]-48 <2 || digits[i]-48> 9)
-                break;
-            temp = mymap[digits[i]-48];
-            vector<string> myvec;
-            for(k=0;k<temp.size();k++){
-                for(j=0;j<ret.size();j++){
-                    myvec.push_back(ret[j]+temp[k]);
+            str=mymap[digits[i]];
+            ret2.clear();
+            for(j=0;j<str.size();j++){
+                for(k=0;k<ret1.size();k++){
+                    temp=ret1[k]+str[j];
+                    ret2.push_back(temp);
                 }
             }
-            ret.swap(myvec);
+            ret1=ret2;
         }
-        return ret;
+        return ret1;
     }
 };
