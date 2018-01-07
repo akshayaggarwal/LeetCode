8d795199b4366011134bdafcc570ff9be1181e1e
Akshay <akadelhi1994@gmail.com>
2018-01-06 23:00:20 -0800

    Sparse Matrix Multiplication

--- Q_311.cpp
+++ Q_311.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
         vector<vector<int>> ret;
-        
-        int i,j,k,num;
-        
+        vector<int> temp;
+        int i,j,k,sum,count;
         for(i=0;i<A.size();i++){
-            
-            ret.push_back(vector<int>());
-            
+            temp.clear();
             for(k=0;k<B[0].size();k++){
-                num = 0;
-            for(j=0;j<B.size();j++){
-                    num += A[i][j]*B[j][k];
+                sum=0;
+                count=0;
+                for(j=0;j<A[0].size();j++){
+                    if(A[i][j]==0)
+                        count++;
+                    sum+=A[i][j]*B[j][k];
                 }
-                
-                ret[i].push_back(num);
+                temp.push_back(sum);
+                if(count==A[0].size()){
+                    k++;
+                    while(k<B[0].size()){
+                        temp.push_back(0);
+                        k++;
+                    }
+                    break;
+                }
             }
+            ret.push_back(temp);
         }
-        
         return ret;
     }
 };
