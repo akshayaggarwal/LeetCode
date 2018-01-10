4809bb1c5718d894d7523c110bb4fe0bbf9e3333
Akshay <akadelhi1994@gmail.com>
2018-01-10 01:18:10 -0800

    Word Break

--- Q_139.cpp
+++ Q_139.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        
-        vector<bool> myvec(s.size()+1,false);
-        myvec[0] = true;
-        
+        map<string,int> mymap;
+        vector<bool> vec(s.size()+1,false);
         int i,j;
-        string str;
-        
+        for(i=0;i<wordDict.size();i++)
+            mymap[wordDict[i]]++;
+        vec[0]=true;
         for(i=1;i<=s.size();i++){
-            
             for(j=i-1;j>=0;j--){
-                
-                if(myvec[j] == true){
-                    
-                    str = s.substr(j,i-j);      // remeber the definition of substr(start_point,len);
-                    if(find(wordDict.begin(),wordDict.end(),str)!=wordDict.end()){
-                        myvec[i] = true;
+                if(vec[j]==true){
+                    if(mymap.find(s.substr(j,i-j))!=mymap.end()){
+                        vec[i]=true;
                         break;
                     }
                 }
             }
-            
         }
-        
-        return myvec[s.size()];
+        return vec[s.size()];
     }
 };
