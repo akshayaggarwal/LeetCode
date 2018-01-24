35860d3561bb4f06035a445808b383d776af1a78
Akshay <akadelhi1994@gmail.com>
2018-01-24 01:54:18 -0800

    Add Two Numbers

--- Q_2.cpp
+++ Q_2.cpp
@@ -9,101 +9,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        vector<int> myvec1,myvec2,ret;
-        
-        int a,i,j,carry;
-        
-        while(l1!=NULL){
-            myvec1.push_back(l1->val);
-            l1 = l1->next;
-        }
-        
-        while(l2!=NULL){
-            myvec2.push_back(l2->val);
-            l2 = l2->next;
-        }
-        
-        reverse(myvec1.begin(),myvec1.end());
-        reverse(myvec2.begin(),myvec2.end());
-        
-        carry = 0;
-        
-        i = myvec1.size()-1;
-        j = myvec2.size()-1;
-        
-        while(i>=0&&j>=0){
-            a = myvec1[i] + myvec2[j] + carry;
-            
-            if(a>9){
-                carry = 1;
-                a = a%10;
-                
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        ListNode *ret = l1;
+        int carry=0;
+        while(l1!=NULL || l2!=NULL){
+            if(l2!=NULL)
+                l1->val=l1->val+l2->val+carry;
+            else
+                l1->val+=carry;
+            if(l1->val>9){
+                carry=l1->val/10;
+                l1->val%=10;
             }
             else
-                carry = 0;
-            ret.push_back(a);
-            i--;
-            j--;
-        }
-    
-        while(i>=0){
-            a = myvec1[i] + carry;
-//            printf("i is %d\n",i);
-            printf("a is %d\n",a);
-            if(a>9){
-                carry = 1;
-                a = a%10;
-                
+                carry=0;
+            if(((l2!=NULL && l2->next!=NULL)||carry!=0) && l1->next==NULL){
+                ListNode *newnode = new ListNode(0);
+                l1->next=newnode;
             }
-            else
-                carry = 0;
-            
-            ret.push_back(a);
-            i--;
-
-        }
-        while(j>=0){
-            a = myvec2[j] + carry;
-            
-            if(a>9){
-                carry = 1;
-                a = a%10;
-                
-            }
-            else
-                carry = 0;
-            
-            ret.push_back(a);
-            j--;
-            //j--;
+            l1=l1->next;
+            if(l2!=NULL)
+                l2=l2->next;
         }
         
-        if(carry == 1)
-            ret.push_back(carry);
-        
-        if(ret.size() == 0)
-            return NULL;
-            
-       // reverse(ret.begin(),ret.end());
-        
-        ListNode *temp;// = new ListNode();
-        ListNode *head = new ListNode(0);
-        
-        temp = head;
-        
-        head->val = ret[0];
-        i = 1;
-        
-        while(i<ret.size()){
-            ListNode *mynode = new ListNode(0);
-            mynode->val = ret[i];
-            head->next = mynode;
-            head = head->next;
-            i++;
-        }
-        
-        return temp;
-        
+        return ret;
     }
 };
