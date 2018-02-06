0a31d40140036877b4806ef208f86471a0b36c6b
Akshay <akadelhi1994@gmail.com>
2018-02-05 16:23:49 -0800

    String Compression

--- Q_443.cpp
+++ Q_443.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i,count,j,totcount=0;
-        char ch;
-        int pos=0;
+        int cur,i,num,count,n;
+        cur=0;
         for(i=0;i<chars.size();){
-            ch = chars[i];
-            chars[pos]=ch;
-            pos++;
-            totcount++;
+            chars[cur]=chars[i];
+            cur++;
             count=0;
-            j=pos;
-            while(i<chars.size()&&chars[i]==ch){
-                i++;
+            while(i<chars.size()&&chars[i]==chars[cur-1]){
                 count++;
+                i++;
             }
             if(count==1)
                 continue;
-            else{
-                int num=0;
-                int k=0;
-                vector<char> numstr;
-                while(count>0){
-                    numstr.push_back((count%10)+48);
-                    count/=10;
-                }
-                reverse(numstr.begin(),numstr.end());
-                while(k<numstr.size()){
-                    chars[j]=numstr[k++];
-                    totcount++;
-                    j++;
-                }
-                
-                pos=j;
+            num=cur;
+            while(count>0){    
+                chars[cur]=(count%10)+48;
+                cur++;
+                count/=10;
             }
+            reverse(chars.begin()+num,chars.begin()+cur);
         }
-        return totcount;
+        return cur;
     }
 };
