7fbd10f8ed414a4f24f57565afa36b88c0ff82ce
Akshay <akadelhi1994@gmail.com>
2018-02-18 23:53:54 -0800

    Valid Word Abbreviation

--- Q_408.cpp
+++ Q_408.cpp
@@ -1,63 +1,28 @@
 class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
-     
-     int i,a;
-     int j;// = 0;
-     
-     for(j=0,i=0;i<abbr.size(),j<word.size();i++,j++)
-     {
-        // printf("i is %d\n",i);
-         if(abbr[i]>='1'&&abbr[i]<='9'){
-             a = 0;
-             
-             while(i<abbr.size() && abbr[i]>='0' && abbr[i]<='9'){
-                 a = a*10 + (abbr[i]-48);
-                 i++;
-             }
-          //  printf("a is %d\n",a);
-            if(a>(word.size()-j))
-                return false;
-            
-            for(int k = 0;k<a && j<word.size();k++,j++);
-            
-            //printf("word[j] is %c\n",word[j]);
-            
-            
-            
-            if(j<word.size() && i<abbr.size()){
-              //  printf("ohoh\n");
-                if(word[j] != abbr[i]){
-                // printf("here\n");
-                    return false;
+        int i,j=0,count,flag;
+        for(i=0;i<abbr.size();i++){
+            if(abbr[i]>='0'&&abbr[i]<='9'){
+                count=0;
+                flag=0;
+                while(abbr[i]>='0'&&abbr[i]<='9'){
+                    if(abbr[i]=='0' && flag==0)
+                        return false;
+                    flag=1;
+                    count=count*10+abbr[i]-48;
+                    i++;
                 }
-            }
-            else if(j>=word.size() && i<abbr.size())
+                i--;
+                j+=count;
+                    continue;
+            }    
+            else if(abbr[i]!=word[j])
                 return false;
-            else if(j<word.size() && i>=abbr.size())
-                return false;
-            //else
-              //  return false;
-         }
-         else{
-             if(abbr[i]!=word[j]){
-              //printf("here\n");
-                return false;
-             }
-         }
-            
-     }
-     
-    // printf("i is %d \n",i);
-    // printf("j is %d \n",j);
-     
-     if(i<abbr.size() || j<word.size()){
-      //printf("here\n");
-      
-        return false;
-     }
-        
-    return true;
-        
+            j++;
+        }
+        if(j<word.size()||j>word.size())
+            return false;
+        return true;
     }
 };
